51da9311973479f89aafbec6048136e64ce1ef9d
btarnow <beckytarnowski@gmail.com>
2023-01-07 16:54:52 -0500

    added routes and pages

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 # "__name__" is a special Python variable for the name of the current module
 # Flask wants to know this to know what any imported things are relative to.
@@ -23,9 +22,35 @@ def show_homepage():
     """Home page."""
 
     return render_template("homepage.html")
-    
 
 
+@app.route('/inherit')
+def show_extend_page_example():
+    """Show example of page that uses base template."""
+
+    return render_template("extend_page_example.html")
+
+@app.route('/hello')
+def show_intro_form():
+    """Show the intro form."""
+
+    return render_template("html_form.html")
+
+@app.route('/greet')
+def show_biography_form_info():
+    """Greet the user"""
+
+    first_name = request.args.get("first_name")
+    last_name = request.args.get("last_name")
+    fav_color = request.args.get("fav_color")
+
+    return render_template("greeting_user.html", 
+                            fav_color = fav_color,
+                            last_name = last_name,
+                            first_name = first_name)
+
+
+#this needs to go at the bottom of your python page: 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
